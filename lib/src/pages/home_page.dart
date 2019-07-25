f712b1d3601c641db94139221490cdaf2e94f4e5
Andres Choque <andreschoquemx0@gmail.com>
2019-07-24 22:36:10 -0500

    Leer codigo QR

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:qrreaderapp/src/pages/direcciones_page.dart';
 import 'package:qrreaderapp/src/pages/mapas_page.dart';
 
+import 'package:qrcode_reader/qrcode_reader.dart';
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -27,12 +29,22 @@ class _HomePageState extends State<HomePage> {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.filter_center_focus),
-        onPressed: (){},
+        onPressed: _scanQR,
         backgroundColor: Theme.of(context).primaryColor,
       ),
     );
   }
+  _scanQR() async{
+    String futureString ='';
+    try{
+      print("asdf");
+      futureString= await new QRCodeReader().scan();
+    }catch(e){
+      futureString=e.toString();
+    } 
+    print('futureString: $futureString');
 
+  }
   Widget _callPage(int paginaActual) {
     switch (paginaActual) {
       case 0:
