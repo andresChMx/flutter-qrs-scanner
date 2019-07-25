acf9d0fb9d3e2dcd52827e7a0ea2cfc675dccc95
Andres Choque <andreschoquemx0@gmail.com>
2019-07-25 11:49:23 -0500

    SQFlite - Crear registros

--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:qrreaderapp/src/models/scan_model.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DBProvider {
@@ -30,5 +31,20 @@ class DBProvider {
             ')'
             );
         });
+  }
+  //crear registros
+  nuevoScanRaw(ScanModel nuevoScan) async{
+    final db=await database;
+    final res=await db.rawInsert(
+      'INSERT INTO Scans (id,tipo,valor) '
+      'VALUES (${nuevoScan.id},${nuevoScan.tipo},${nuevoScan.valor})'
+    );
+    return res;//retorna numero de inserciones realizadas (1 en este caso)
+  }
+  //crear registro de forma mas facil,rapida,segura
+  nuevoScan(ScanModel nuevoScan) async{
+    final db=await database;
+    final res=await db.insert('Scans', nuevoScan.toJson());
+    return res;
   }
 }
