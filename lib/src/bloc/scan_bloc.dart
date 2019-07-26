73f67f0cb4548891c751a23182136b2ceca6c201
Andres Choque <andreschoquemx0@gmail.com>
2019-07-26 18:00:51 -0500

    Metodos para controlar el fujo de informacion

--- lib/src/bloc/scan_bloc.dart
+++ lib/src/bloc/scan_bloc.dart
@@ -8,11 +8,25 @@ class ScansBloc{
     return _singleton;
   }
   ScansBloc._internal(){
-
+    obtenerScans();
   }
   final _scansController=StreamController<List<ScanModel>>.broadcast(); 
   Stream<List<ScanModel>>get scansStream=>_scansController.stream;
   dispose(){
     _scansController?.close();
+  }
+  agregarScans(ScanModel scan)async{
+    await DBProvider.db.nuevoScan(scan); //await porsiaca
+    obtenerScans();
+  }
+  obtenerScans() async{
+    _scansController.sink.add(await DBProvider.db.getTodosScans());
+  }
+  borrarScan(int id) async{
+    await DBProvider.db.deleteScan(id);
+    obtenerScans();
+  }
+  borrarScanTodos()async{
+    await DBProvider.db.deleteAll();
   }
 }
